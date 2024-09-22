c3f66f4ae02bea65a2829bb5c5d49905dfec481c
leoWeitz <lweitz@itba.edu.ar>
2024-09-22 19:25:56 -0300

    allocMemory

--- MemoryManagerCDT.c
+++ MemoryManagerCDT.c
@@ -11,6 +11,7 @@ struct MemoryManagerCDT
     struct Block *free_blocks;
     void *mem_start;
     size_t total_size;
+    size_t continuous_free_space;
 };
 
 //@TODO: modularizar líneas 20-22 / 29-31 (asignado de variables de struct Block)
@@ -19,24 +20,44 @@ MemoryManagerADT createMemoryManager(void *const memoryForMemoryManager, void *c
     MemoryManagerADT mm = (MemoryManagerADT)memoryForMemoryManager;
     mm->mem_start = managedMemory;
     mm->total_size = TOTAL_MEM;
-    mm->free_blocks = (struct Block *)managedMemory + sizeof(struct MemoryManagerCDT); // + sizeof(struct MemoryManagerCDT) ?
-    mm->free_blocks->next = NULL;                                                      // mm->free_blocks + sizeof(struct Block) + BLOCK_SIZE; //+1?
+
+    mm->free_blocks = (struct Block *)(memoryForMemoryManager + sizeof(struct MemoryManagerCDT)); // @TODO: chequear
+    mm->free_blocks->size = TOTAL_MEM;
+    mm->free_blocks->next = NULL;
 }
 
 void *allocMemory(MemoryManagerADT memoryManager, size_t memoryToAllocate)
 {
+    if (memoryToAllocate == 0)
+    {
+        return NULL;
+    }
+
+    // iterar por la lista de bloques libres hasta encontrar bloques con suficiente tamaño
+    int found = 0;
     struct Block *currentBlock = memoryManager->free_blocks;
+    struct Block *previousBlock = currentBlock;
+    do
+    {
+        if (currentBlock->size > memoryToAllocate + sizeof(struct Block)) // Para simplificar, si chequeamos después se divide en más casos
+        {
+            // Sacar currentBlock de la lista de bloques libres
+            // Que el anterior apunte a currentBlock + sizeof(struct Block) + memoryToAllocate
+            previousBlock->next = currentBlock + sizeof(struct Block) + memoryToAllocate; // Asegurado que prevBlock->next tiene espacio disponible
+            previousBlock->next->next = currentBlock->next;
+            previousBlock->next->size = currentBlock->size - sizeof(struct Block) - memoryToAllocate;
+            found = 1;
+        }
+        else
+        {
+            // Paso al siguiente bloque
+            previousBlock = currentBlock;
+            currentBlock = currentBlock->next;
+        }
 
-    // if (memoryManager->free_blocks->next != NULL)
-    //     memoryManager->free_blocks = (struct Block *)memoryManager->free_blocks->next;
-    // else
-    //     memoryManager->free_blocks += sizeof(struct Block) + BLOCK_SIZE;
-    memoryManager->free_blocks->size = BLOCK_SIZE;
+    } while (!found);
 
-    memoryManager->free_blocks->next = memoryManager->free_blocks + sizeof(struct Block) + BLOCK_SIZE; //+1?
-
-    // return (void *)(blockToGive + sizeof(struct Block));
-    return NULL;
+    return (void *)currentBlock + sizeof(struct Block);
 }
 
 void freeMemory(MemoryManagerADT mm, void *ptr)
